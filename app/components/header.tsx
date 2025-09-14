d7ee63b63eeb0e2e450b52b0321e9320186ef733
Rohan Iyer <67619222+Kazumin7@users.noreply.github.com>
2025-09-14 06:09:37 -0700

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
       whileInView={{y: 0, opacity: 1}}
       transition={{duration: 0.6, delay: 0.3}}
       className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
-            Hi! I'm Rohan Iyer
+        {"Hi! I'm Rohan Iyer"}
             <Image src={assets.hand_icon} alt='' className='rounded-full w-6'/></motion.h3>
 
         <motion.h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'
